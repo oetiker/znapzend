978901c10615eda118bbf2efb3d42b33e871fdef
hadfl <hadfl@users.noreply.github.com>
2014-06-03 20:17:27 +0200

    - life's too short not to have a bit of fun from time to time...

--- lib/ZnapZend.pm
+++ lib/ZnapZend.pm
@@ -145,6 +145,13 @@ sub start {
         my ($timeStamp, $actionList) =  $self->zTime->getActionList($self->backupSets);    
 
         my $timeToWait = $timeStamp - time();
+        if ($cleanUp){
+            sleep($timeToWait > $self->forkPollInterval ? $self->forkPollInterval : $timeToWait);
+        }
+        else{
+            syslog('info', "nothing to do for me. am so bored... off for a coffee break. will be back in $timeToWait seconds to serve you, my master");
+            sleep($timeToWait);
+        }
         sleep($cleanUp ? ($timeToWait > $self->forkPollInterval ? $self->forkPollInterval : $timeToWait) : $timeToWait);
 
         # check if we need to snapshot, since we start polling if child is active and might be early
