f94f87c0171691129ca64dcdc95e1d13e8224b2b
hadfl <hadfl@users.noreply.github.com>
2014-07-01 10:39:09 +0200

    - fixed missing ;

--- lib/ZnapZend/Time.pm
+++ lib/ZnapZend/Time.pm
@@ -116,12 +116,12 @@ sub backupPlanToHash {
         my @planValues = split '=>', $planItem, 2;
         my ($value, $unit) = $planValues[0] =~ /(\d+)([a-z]+)/;
         $value && exists $self->unitFactors->{$unit}
-            or die "ERROR: backup plan $backupPlan is not valid\n"
+            or die "ERROR: backup plan $backupPlan is not valid\n";
 
         my $key = $self->$timeToTimestamp($value, $unit);
         ($value, $unit) = $planValues[1] =~ /(\d+)([a-z]+)/;
         $value && exists $self->unitFactors->{$unit}
-            or die "ERROR: backup plan $backupPlan ist not valid\n"
+            or die "ERROR: backup plan $backupPlan ist not valid\n";
 
         $backupPlan{$key} = $self->$timeToTimestamp($value, $unit);
     }
