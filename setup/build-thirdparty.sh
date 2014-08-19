f593204e7b48035df2ba5b53cd91acd58f70e956
Tobias Oetiker <tobi@oetiker.ch>
2014-08-19 14:20:34 +0200

    get ready for 0.12.0

--- setup/build-thirdparty.sh
+++ setup/build-thirdparty.sh
@@ -2,9 +2,10 @@
 
 . `dirname $0`/sdbs.inc
 
+#  Sys::Syslog \
+
 for module in \
-  Sys::Syslog \
-  Mojolicious@5.21 \
+  Mojolicious@5.30 \
   Mojo::IOLoop::ForkCall@0.14 \
 ; do
 perlmodule $module
