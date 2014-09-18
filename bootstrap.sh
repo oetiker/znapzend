6a4ab4b6585b629454c4d84c4d722b787d9f0e0c
Tobias Oetiker <tobi@oetiker.ch>
2014-09-18 08:21:00 +0200

    prepare for 0.13.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 autoreconf --force --install --verbose --make
+git log --full-history --simplify-merges --dense --no-merges > CHANGES
 # EOF
--- setup/build-thirdparty.sh
+++ setup/build-thirdparty.sh
@@ -6,7 +6,6 @@
 for module in \
   Mojolicious@5.30 \
   Mojo::IOLoop::ForkCall@0.14 \
-  Sys::Syslog \
 ; do
 perlmodule $module
 done
