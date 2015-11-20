cd5330c97d9b024fc8b5af32f2435d757a254c52
morph027 <morphsen@gmx.com>
2015-11-20 16:33:11 +0100

    added subdir

--- checkinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-make clean
-./configure --prefix=/usr
-make
-sudo checkinstall --install=no --pkgname=znapzend --pkgversion=$(git describe --abbrev=0 --tags | sed 's,^v,,') --pkglicense=GPL --pkgrelease=1 --pkgsource="https://github.com/oetiker/znapzend" --requires="zfsutils,mbuffer" --provides=znapzend --nodoc
