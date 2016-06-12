16c31a8d31c8822ea992954ab6aac9fd2a9a5fb1
L.C. Karssen <l.c.karssen@polyomica.com>
2016-06-12 16:17:33 +0200

    Fix issue #220, detection of zfsutils package in Ubuntu
    This fixes issue #220. Previously, the script automatically added
    `zfsutils` as dependecy. In Ubuntu 16.04 the package is called
    zfsutils-linux. This commit runs `lsb_release -r` to get the release
    number and then checks if it is running on 16.04 or greater to set the
    correct zfsutils dependency.
    
    Note, this commit was only tested on an Ubuntu machine. It may need
    editing for Debian or other distros.

--- packaging/checkinstall/checkinstall.sh
+++ packaging/checkinstall/checkinstall.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Exit when encountering an undefined variable
+set -u
+
+# Determine the Ubuntu version we are running on. Versions before
+# 16.04 had a package called zfsutils, from 16.04 onwards the package
+# is called zfsutils-linux
+DistroMajorVersion=$(lsb_release -r |cut -f2 | cut -d. -f1)
+DistroMinorVersion=$(lsb_release -r |cut -f2 | cut -d. -f2)
+if [ ${DistroMajorVersion} -ge 16 ] && [ ${DistroMinorVersion} -ge 4 ]; then
+    zfsutilsdep="zfsutils-linux"
+else
+    zfsutilsdep="zfsutils"
+fi
+
+# Compile znapzend
 make clean
 ./configure --prefix=/usr
 make
+
+# Create the package
 ln -s packaging/checkinstall/*-pak .
-sudo checkinstall --install=no --pkgname=znapzend --pkgversion=$(git describe --abbrev=0 --tags | sed 's,^v,,') --pkglicense=GPL --pkgrelease=1 --pkgsource="https://github.com/oetiker/znapzend" --requires="zfsutils,mbuffer" --provides=znapzend --nodoc --backup=no
+sudo checkinstall --install=no --pkgname=znapzend --pkgversion=$(git describe --abbrev=0 --tags | sed 's,^v,,') --pkglicense=GPL --pkgrelease=1 --pkgsource="https://github.com/oetiker/znapzend" --requires="${zfsutilsdep},mbuffer" --provides=znapzend --nodoc --backup=no
 rm -f *-pak
