00247589ed90644cb084a2092c6b1fbad2669167
Malcolm Scott <github@malc.org.uk>
2018-11-04 15:24:19 +0000

    Don't use deprecated Mojo::Util::slurp

--- lib/ZnapZend.pm
+++ lib/ZnapZend.pm
@@ -2,7 +2,7 @@ package ZnapZend;
 
 use Mojo::Base -base;
 use Mojo::IOLoop::ForkCall;
-use Mojo::Util qw(slurp);
+use Mojo::File;
 use Mojo::Log;
 use ZnapZend::Config;
 use ZnapZend::ZFS;
@@ -638,7 +638,7 @@ my $daemonize = sub {
     my $pidFile = $self->pidfile || $self->defaultPidFile;
 
     if (-f $pidFile){
-        chomp(my $pid = slurp $pidFile);
+        chomp(my $pid = Mojo::File->new($pidFile)->slurp);
         #pid is not empty and is numeric
         if ($pid && ($pid = int($pid)) && kill 0, $pid){
             die "I Quit! Another copy of znapzend ($pid) seems to be running. See $pidFile\n";
