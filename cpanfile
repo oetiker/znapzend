ad4a2502ee2706e7946fc7200cf9232495d5f30d
Tobias Oetiker <tobi@oetiker.ch>
2022-12-20 09:30:40 +0100

    better test ?

--- cpanfile
+++ cpanfile
@@ -5,3 +5,4 @@ requires 'Scalar::Util', '>= 1.45';
 requires 'Test::SharedFork';
 requires 'Test::Exception';
 requires 'Test::More';
+requires 'Devel::Cover';
--- test.sh
+++ test.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 # Fail on a test line that dies so we can notice it
 set -e
 
 perl -I./thirdparty/lib/perl5 \
-  -MDevel::Cover=+ignore,thirdparty ./t/znapzend.t ./bin/znapzend || test $? == 1
+  -MDevel::Cover=+ignore,thirdparty ./t/znapzend.t
 perl -I./thirdparty/lib/perl5 \
-  -MDevel::Cover=+ignore,thirdparty ./t/znapzend-daemonize.t ./bin/znapzend || test $? == 1
+  -MDevel::Cover=+ignore,thirdparty ./t/znapzend-daemonize.t
 perl -I./thirdparty/lib/perl5 \
-  -MDevel::Cover=+ignore,thirdparty ./t/znapzendzetup.t ./bin/znapzend || test $? == 1
+  -MDevel::Cover=+ignore,thirdparty ./t/znapzendzetup.t 
 perl -I./thirdparty/lib/perl5 \
-  -MDevel::Cover=+ignore,thirdparty ./t/znapzendztatz.t ./bin/znapzend || test $? == 1
+  -MDevel::Cover=+ignore,thirdparty ./t/znapzendztatz.t
 perl -I./thirdparty/lib/perl5 \
-  -MDevel::Cover=+ignore,thirdparty ./t/autoscrub.t ./bin/znapzend || test $? == 1
+  -MDevel::Cover=+ignore,thirdparty ./t/autoscrub.t
 
-true
