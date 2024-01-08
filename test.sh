3c7d3007cb5841e85d2418cb184896c9859fe0c7
Jim Klimov <jimklimov@gmail.com>
2024-01-08 16:45:19 +0000

    test.sh: call t/znapzend-lib-splitter.t (but ignore faults for now) [#585]
    Signed-off-by: Jim Klimov <jimklimov@gmail.com>

--- test.sh
+++ test.sh
@@ -8,9 +8,13 @@ perl -I./thirdparty/lib/perl5 \
 perl -I./thirdparty/lib/perl5 \
   -MDevel::Cover=+ignore,thirdparty ./t/znapzend-daemonize.t
 perl -I./thirdparty/lib/perl5 \
-  -MDevel::Cover=+ignore,thirdparty ./t/znapzendzetup.t 
+  -MDevel::Cover=+ignore,thirdparty ./t/znapzendzetup.t
 perl -I./thirdparty/lib/perl5 \
   -MDevel::Cover=+ignore,thirdparty ./t/znapzendztatz.t
 perl -I./thirdparty/lib/perl5 \
   -MDevel::Cover=+ignore,thirdparty ./t/autoscrub.t
 
+# Currently prone to failure with certain edge cases,
+# so ignoring the result (fixes are investigated):
+perl -I./thirdparty/lib/perl5 \
+  -MDevel::Cover=+ignore,thirdparty ./t/znapzend-lib-splitter.t || echo "FAILURE Currently ignored"
