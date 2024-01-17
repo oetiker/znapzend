07b8b149952fd83bb44e561c6e7708e47385a10d
Jim Klimov <jimklimov@gmail.com>
2024-01-17 14:56:26 +0000

    lib/ZnapZend/Config.pm: checkBackupSets(): do not assign "undef" src_mbuffer, fall back to "off" [#629]
    Signed-off-by: Jim Klimov <jimklimov@gmail.com>

--- lib/ZnapZend/Config.pm
+++ lib/ZnapZend/Config.pm
@@ -217,7 +217,7 @@ my $checkBackupSets = sub {
         if ($backupSet->{src_mbuffer}) {
             if ($backupSet->{src_mbuffer} ne 'off' and !($self->zfs->fileExistsAndExec($backupSet->{src_mbuffer}))) {
                 warn "*** WARNING: executable '$backupSet->{src_mbuffer}' does not exist on source system, will ignore\n\n";
-                $backupSet->{src_mbuffer} = undef;
+                $backupSet->{src_mbuffer} = 'off';
             }
         }
         if (!($backupSet->{src_mbuffer})) {
