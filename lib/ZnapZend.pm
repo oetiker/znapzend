9c86890aac49f8ddc48863787a1676800799c5a2
Jim Klimov <jimklimov@gmail.com>
2024-03-12 09:08:29 +0000

    lib/ZnapZend.pm: sendRecvCleanup(): clarify the message when we skip a not-enabled dataset
    Signed-off-by: Jim Klimov <jimklimov@gmail.com>

--- lib/ZnapZend.pm
+++ lib/ZnapZend.pm
@@ -601,7 +601,7 @@ my $sendRecvCleanup = sub {
             $dstDataSet =~ s/^\Q$backupSet->{src}\E/$backupSet->{$dst}/;
 
             my $srcDataSetDisabled = (grep (/^\Q$srcDataSet\E$/, @dataSetsExplicitlyDisabled));
-            $self->zLog->debug('sending snapshots from ' . $srcDataSet . ' to ' . $dstDataSet . ($srcDataSetDisabled ? ": not enabled, should be skipped" : ""));
+            $self->zLog->debug('sending snapshots from ' . $srcDataSet . ' to ' . $dstDataSet . ($srcDataSetDisabled ? ": not enabled, skipped" : ""));
             if ($srcDataSetDisabled) {
                 next;
             }
