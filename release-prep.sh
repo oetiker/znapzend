dafc1a00ba15846bce3ebc89ba55edd79f8eb1b1
Tobias Oetiker <tobi@oetiker.ch>
2024-06-14 14:43:11 +0200

    prep for 0.23.0

--- release-prep.sh
+++ release-prep.sh
@@ -5,17 +5,11 @@ P=znapzend
 rm -f config.status
 ./bootstrap.sh
 ./configure
-echo ${V} `date +"%Y-%m-%d %H:%M:%S %z"` `git config user.name` '<'`git config user.email`'>' > CHANGES.new
-echo >> CHANGES.new
-echo ' -' >> CHANGES.new
-echo >> CHANGES.new
-$EDITOR CHANGES.new
-tail --line=+2 CHANGES.new | sed -e 's/^  //' > release-notes-$V.md
-cat CHANGES >> CHANGES.new
-mv CHANGES.new CHANGES
+VERSION=$(cat VERSION)
+debchange -m -v $V
 make
 make dist
-git checkout -b v${V}
+git checkout -b v${V} || true
 git commit -a
 git push --set-upstream origin v${V}
 #gh release create v${V} --title "ZnapZend $V" --notes-file release-notes-$V.md ${P}-${V}.tar.gz'#Source Archive' 
