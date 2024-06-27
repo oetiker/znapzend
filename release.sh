07cb8c434fa69420f6f8f5c4549190c9bb87f1d9
Tobias Oetiker <tobi@oetiker.ch>
2024-06-27 12:10:57 +0200

    release 0.23.2 to build ubuntu 24.4 deb

--- release.sh
+++ release.sh
@@ -13,3 +13,4 @@ git checkout -b v${V} || true
 git commit -a
 git push --set-upstream origin v${V}
 #gh release create v${V} --title "ZnapZend $V" --notes-file release-notes-$V.md ${P}-${V}.tar.gz'#Source Archive' 
+gh release create v${V} --title "ZnapZend $V" ${P}-${V}.tar.gz
